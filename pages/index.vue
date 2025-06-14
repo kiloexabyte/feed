edd7efec8181c5dfd1086e0a04035f9a704b16aa
kiloexabyte <vothanhdat278+1@gmail.com>
2025-06-14 18:43:26 -0500

    add category to news article

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,9 @@ function formatDate(dateStr) {
 		<h1 class="mb-4 text-2xl font-bold">News Feed</h1>
 		<ul class="space-y-4">
 			<li v-for="item in articles" :key="item.link" class="w-1/2 border-b pb-2">
+				<div>
+					{{ item.category }}
+				</div>
 				<Article
 					:title="item.title"
 					:author="item.author"
