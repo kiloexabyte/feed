84ae80d7cb91284ab876d54c6a7e25aa0cd687d4
kiloexabyte <vothanhdat278+1@gmail.com>
2025-06-14 18:05:37 -0500

    extract out article component

---
+++ components/Article.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+type ArticleProps = {
+    title: string;
+    author: string;
+    publishedDate: string;
+    summary: string;
+    link: string;
+};
+
+const props = defineProps<ArticleProps>();
+</script>
+
+<template>
+    <a :href="props.link" target="_blank" class="text-lg font-semibold hover:underline">
+        {{ props.title }}
+    </a>
+    <div class="text-sm text-gray-500">
+        {{ props.author }} · {{ props.publishedDate }}
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -11,13 +11,14 @@ function formatDate(dateStr) {
   <div class="p-6">
     <h1 class="text-2xl font-bold mb-4">News Feed</h1>
     <ul class="space-y-4">
-      <li v-for="item in articles" :key="item.link" class="border-b pb-2">
-        <a :href="item.link" target="_blank" class="text-lg font-semibold hover:underline">
-          {{ item.title }}
-        </a>
-        <div class="text-sm text-gray-500">
-          {{ item.author }} · {{ formatDate(item.date) }}
-        </div>
+      <li v-for="item in articles" :key="item.link" class="border-b pb-2 w-1/2">
+        <Article
+            :title="item.title"
+            :author="item.author"
+            :published-date="formatDate(item.date)"
+            :summary="item.summary"
+            :link="item.link"
+        />
       </li>
     </ul>
   </div>
