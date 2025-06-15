ccbb12cf990b7f8f6d1296b175e11c49e0cbf55d
kiloexabyte <vothanhdat278+1@gmail.com>
2025-06-14 22:14:46 -0500

    extract video into its on component

--- components/VideoItem.vue
+++ components/VideoItem.vue
@@ -5,6 +5,35 @@ type VideoItemProps = {
 	author: string;
 	link: string;
 };
+
+const props = defineProps<VideoItemProps>();
+
+// Extract YouTube video ID from the video link
+function getYouTubeVideoId(link: string) {
+	const url = new URL(link);
+	return url.searchParams.get("v") || url.pathname.split("/").pop();
+}
 </script>
 
-<template></template>
+<template>
+	<div class="mb-2">
+		<a
+			:href="props.link"
+			target="_blank"
+			class="text-lg font-semibold hover:underline"
+		>
+			{{ props.title }}
+		</a>
+	</div>
+	<div class="mb-2 text-sm text-gray-500">
+		{{ props.author }} · {{ props.publishedDate }}
+	</div>
+	<!-- Embed YouTube video -->
+	<iframe
+		:src="`https://www.youtube.com/embed/${getYouTubeVideoId(props.link)}`"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+		allowfullscreen
+		class="aspect-video w-full"
+	></iframe>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,6 @@ function formatDate(dateStr) {
 	const date = new Date(dateStr);
 	return isNaN(date) ? "Unknown date" : date.toLocaleDateString();
 }
-
-// Extract YouTube video ID from the video link
-function getYouTubeVideoId(link) {
-	const url = new URL(link);
-	return url.searchParams.get("v") || url.pathname.split("/").pop();
-}
 </script>
 
 <template>
@@ -25,17 +19,13 @@ function getYouTubeVideoId(link) {
 						:key="item.link"
 						class="w-full border-b pb-2"
 					>
-						<div>
-							{{ item.category }}
-						</div>
-						<div class="w-full">
-							<Article
-								:title="item.title"
-								:author="item.author"
-								:published-date="formatDate(item.date)"
-								:link="item.link"
-							/>
-						</div>
+						<Article
+							:title="item.title"
+							:author="item.author"
+							:publishedDate="formatDate(item.date)"
+							:link="item.link"
+							:category="item.category"
+						/>
 					</li>
 				</ul>
 			</div>
@@ -50,26 +40,12 @@ function getYouTubeVideoId(link) {
 						:key="video.link"
 						class="w-full border-b pb-4"
 					>
-						<div class="mb-2">
-							<a
-								:href="video.link"
-								target="_blank"
-								class="text-lg font-semibold hover:underline"
-							>
-								{{ video.title }}
-							</a>
-						</div>
-						<div class="mb-2 text-sm text-gray-500">
-							{{ video.author }} · {{ formatDate(video.date) }}
-						</div>
-						<!-- Embed YouTube video -->
-						<iframe
-							:src="`https://www.youtube.com/embed/${getYouTubeVideoId(video.link)}`"
-							frameborder="0"
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-							allowfullscreen
-							class="aspect-video w-full"
-						></iframe>
+						<VideoItem
+							:title="video.title"
+							:publishedDate="formatDate(video.publishedDate)"
+							:author="video.author"
+							:link="video.link"
+						/>
 					</li>
 				</ul>
 			</div>
